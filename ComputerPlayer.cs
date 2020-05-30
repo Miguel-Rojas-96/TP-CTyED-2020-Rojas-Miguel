e2d8915d09ca42610d9227490fee2eeab727755e
Miguel-Rojas-1996 <bk_magnus@hotmail.com>
2020-05-30 17:54:51 +0200

    Mi Metodo Inicializar!
    -En la presente modificacion se realizo el armado de nuestro arbol general. El mismo tendra todas las jugadas posibles que pueden llegar a aparecer durante una partida.

--- ComputerPlayer.cs
+++ ComputerPlayer.cs
@@ -12,9 +12,49 @@ namespace juegoIA
 		{
 		}
 		
-		public override void  incializar(List<int> cartasPropias, List<int> cartasOponente, int limite)
+		public override void incializar(List<int> cartasPropias, List<int> cartasOponente, int limite)
 		{
-			//Implementar
+			bool Turno=true; //comienza tirando el usuario!
+			List<int> usuario=new List<int>(){1,2};
+			List<int> maquina=new List<int>(){3,4};			
+			ArbolGeneral<int> ArbolCompleto=CompletarRama(new ArbolGeneral<int>(0),usuario,maquina,Turno,limite);
+			ArbolCompleto.PorNivelesMarcadoFin();
+
+		}
+		public ArbolGeneral<int> CompletarRama(ArbolGeneral<int> Hijo,List<int> CartasUsuario,List<int> CartasMaquina,bool Turno,int limite)
+		{
+			List<int> cartas=new List<int>();
+			if(Turno==true)
+			{
+				cartas.AddRange(CartasUsuario);
+			}
+			else
+			{
+				cartas.AddRange(CartasMaquina);
+			}
+			foreach(int carta in cartas)
+			{
+
+				ArbolGeneral<int> hijo=new ArbolGeneral<int>(carta);
+				Hijo.agregarHijo(hijo);
+				int limiteaAux=limite-carta;
+				List<int> cartasrestantes=new List<int>();
+				cartasrestantes.AddRange(cartas);
+				cartasrestantes.Remove(carta);
+				if(limite>=0)
+				{
+					if(Turno==true)
+					{
+						bool turnito=false;
+						CompletarRama(hijo,cartasrestantes,CartasMaquina,turnito,limiteaAux);
+					}
+					else{
+						bool turnito=true;
+						CompletarRama(hijo,CartasUsuario,cartasrestantes,turnito,limiteaAux);
+					}
+				}
+			}
+			return Hijo;
 		}
 		
 		
